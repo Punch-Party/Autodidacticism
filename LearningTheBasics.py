bb3e566c879d8e4b268638c8eec9cc0080f96444
Punch_Party <Karambelas13@gmail.com>
2018-02-04 01:38:21 -0500

    'Hello World' run and using a sentinal value controlled while loop to print 'Hello World' five times.

--- LearningTheBasics.py
+++ LearningTheBasics.py
@@ -2,4 +2,17 @@
 # Coder: Punch_Party
 # Email: karambelas13@gmail.com
 
+# First timer classic
 print('Hello World')
+
+# Using characters to separate out different codeblocks
+print('\n#######################\n')
+
+# Sentinal value controlled while loop to ouptut
+# the classic 5 times
+x = 0
+while x < 5:
+    print('Hello World')
+    x += 1
+
+
